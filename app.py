458b4a0ee42317bba09810448bb6c97d392756bc
preston <preston@erissoft.com>
2024-04-01 18:53:08 -0400

    Changes

--- app.py
+++ app.py
@@ -1,7 +1,19 @@
+from fastai.vision.all import *
 import gradio as gr
 
-def greet(name):
-    return "Hello " + name + "!!"
+LEARNER = load_learner("Akash_Food_or_Not.pkl")
 
-iface = gr.Interface(fn=greet, inputs="text", outputs="text")
+def will_akash_eat(learn) -> bool:
+    def will_akash_eat_inner(image_name):
+        food_type, _, _ = learn.predict(image_name)
+        if food_type == 'indian_food':
+            return True
+        else:
+            return False
+    return will_akash_eat_inner
+
+image = gr.inputs.Image(shape=(192,192))
+label = gr.outputs.Label()
+
+iface = gr.Interface(fn=will_akash_eat(LEARNER), inputs=image, outputs=label)
 iface.launch()
