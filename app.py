55bdddf9df6fda79eca0f000ddb0538e54b7f832
preston <preston@erissoft.com>
2024-04-01 20:28:52 -0400

    small fixes

--- app.py
+++ app.py
@@ -5,15 +5,14 @@ LEARNER = load_learner("Akash_Food_or_Not.pkl")
 
 def will_akash_eat(learn) -> bool:
     def will_akash_eat_inner(image_name):
-        food_type, _, _ = learn.predict(image_name)
-        if food_type == 'indian_food':
-            return True
-        else:
-            return False
+        labels = learn.dls.vocab
+        pred, idx, probs = learn.predict(image_name)
+        return {labels[i]: float(probs[i]) for i in range(len(labels))}
     return will_akash_eat_inner
 
 image = gr.components.Image(shape=(192,192))
 label = gr.components.Label
+examples = ['example_pictures/butter_chicken.png', 'example_pictures/samosa.png', 'example_pictures/burger.png']
 
-iface = gr.Interface(fn=will_akash_eat(LEARNER), inputs=image, outputs=label)
+iface = gr.Interface(fn=will_akash_eat(LEARNER), inputs=image, outputs=label, examples=examples)
 iface.launch()
