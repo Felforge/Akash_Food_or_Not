3764ab0f4e7c1ab5dc801e6cef37eb6c3ea3da78
preston <preston@erissoft.com>
2024-04-01 21:10:42 -0400

    minor fixes

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ def will_akash_eat(learn) -> bool:
         return {labels[i]: float(probs[i]) for i in range(len(labels))}
     return will_akash_eat_inner
 
-image = gr.Image(shape=(192,192))
+image = gr.Image(height=192, width=192)
 label = gr.Label()
 examples = ['example_pictures/butter_chicken.png', 'example_pictures/samosa.png', 'example_pictures/burger.png']
 
